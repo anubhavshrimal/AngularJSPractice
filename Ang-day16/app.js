68e00a03780811dbedc707d7793b016e54d201d2
anubhavshrimal <anubhavshrimal@gmail.com>
2016-06-07 10:21:22 +0530

    localForage method of data storage in browser, added ResourcesUsed for all the libraries and files required

--- Ang-day16/app.js
+++ Ang-day16/app.js
@@ -1,17 +1,24 @@
-var app = angular.module("myApp",[]);
+var app = angular.module("myApp",["LocalForageModule"]);
 
-app.controller("firstCtrl", function ($scope) {
+app.controller("firstCtrl", function ($scope, $localForage) {
     $scope.sendData = function () {
-        var obj = JSON.stringify($scope.user);      // JSON -> String format as data is set in String only
-        localStorage.setItem("user", obj);          // Store the string format of object in local storage
-        console.log("item stored");
+        /*var obj = JSON.stringify($scope.user);      // JSON -> String format as data is set in String only
+        localStorage.setItem("user", obj);          // Store the string format of object in local storage*/
+        $localForage.setItem("user", $scope.user).then(function () { // Set item using LocalForage
+            console.log("item stored");
+        });
+
     }
 
     $scope.getData = function () {
-        $scope.value = JSON.parse(localStorage.getItem("user"));    // String format -> JSON
+        // $scope.value = JSON.parse(localStorage.getItem("user"));    // String format -> JSON
+        $localForage.getItem("user").then(function (data) {
+            $scope.value = data;
+        });
         console.log("item fetched");
     }
-    
+                    //for array user Array.stringify and Array.parse
+
     $scope.removeData = function () {
         localStorage.removeItem("user");
         console.log("item removed")
