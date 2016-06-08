d9148c9c098a131b4e9077488ba3330690f89581
anubhavshrimal <anubhavshrimal@gmail.com>
2016-06-08 11:16:22 +0530

    added underscore.js an iteration library

--- Ang-day17/under.js
+++ Ang-day17/under.js
@@ -6,21 +6,33 @@ var persons = [
     {
         "name": "anubhav",
         "college": "skit",
-        "age": 20
+        "age": 5
     },
     {
         "name": "ritika",
-        "college": "iit",
-        "age": 20
+        "college": "skit",
+        "age": 12
     },
     {
         "name": "aditya",
         "college": "poornima",
-        "age": 20
+        "age": 0
     },
     {
         "name": "harsh",
         "college": "jecrc",
-        "age": 21
+        "age": 4
     }
 ];
+
+var sorted = _.sortBy(persons, function (p) {
+    return -p.age;
+});
+
+console.log("sorted by age in descending order",sorted);
+
+var grouped = _.groupBy(persons, function (p) {
+    return p.college;
+});
+
+console.log("grouped by college",grouped);
