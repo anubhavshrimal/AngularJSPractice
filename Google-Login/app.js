15f9aa0b0b33c45ed8d9f4fdb24c7a8ee532e72b
anubhavshrimal <anubhavshrimal@gmail.com>
2016-06-14 11:41:19 +0530

    google authentication added in Google-Login

--- Google-Login/app.js
+++ Google-Login/app.js
@@ -1,8 +1,17 @@
 var app = angular.module("myApp",["firebase"]);
 
-app.controller("firstCtrl", function ($scope, $firebaseArray) {
+app.controller("firstCtrl", function ($scope, $firebaseArray, $firebaseAuth) {
 
-    var ref = new Firebase("https://anubhav.firebaseio.com/colleges");
+    var ref = new Firebase("https://anubhav.firebaseio.com/");
 
-    $scope.colleges = $firebaseArray(ref);
+    // create an instance of the authentication service
+    var auth = $firebaseAuth(ref);
+    // login with Facebook
+    auth.$authWithOAuthPopup("google").then(function(authData) {
+        console.log("Logged in as:", authData.uid);
+        $scope.url = authData.google.profileImageURL;
+        $scope.name = authData.google.displayName;
+    }).catch(function(error) {
+        console.log("Authentication failed:", error);
+    });
 });
