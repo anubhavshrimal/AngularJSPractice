7bbb403df2ef223ddf3bdc0aff3be9a0603c1bba
anubhavshrimal <anubhavshrimal@gmail.com>
2016-06-16 10:46:53 +0530

    added ngClass

---
+++ ngClass/app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by anubhavshrimal on 13/6/16.
+ */
+
+var app = angular.module("myApp", []);
+
+'use strict';
+
+app.controller("firstCtrl", ["$scope", function ($scope) {
+
+    this.style = {
+        "background": "yellow"
+    }
+}]);
